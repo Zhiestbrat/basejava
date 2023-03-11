4c15b8e6322e074aa10ac6a45a8bbd867fe1a87c
Zhiestbrat <fffcondakov@yandex.ru>
2023-03-11 21:19:04 +0500

    hw5_vs1

---
+++ src/com/urise/webapp/storage/ListStorage.java
@@ -0,0 +1,12 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListStorage {
+    private final List<Resume> listResume = new ArrayList<>();
+
+
+}
