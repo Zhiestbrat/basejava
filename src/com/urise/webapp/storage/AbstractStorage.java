167e75dc28b8fd88a37a9af4fdfd1ac7f9659678
Zhiestbrat <fffcondakov@yandex.ru>
2023-03-16 20:50:44 +0500

    hw6_vs2

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -4,6 +4,7 @@ import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.model.Resume;
 
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -29,13 +30,7 @@ public abstract class AbstractStorage<E> implements Storage {
 
     public final List<Resume> getAllSorted() {
         List<Resume> list = getList();
-        list.sort((o1, o2) -> {
-            if (o1.getFullName().equals(o2.getFullName())) {
-                return o1.getUuid().compareTo(o2.getUuid());
-            } else {
-                return o1.getFullName().compareTo(o2.getFullName());
-            }
-        });
+        list.sort(Comparator.comparing(Resume::getFullName).thenComparing(Resume::getUuid));
         return list;
     }
 
